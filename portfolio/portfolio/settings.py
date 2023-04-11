c34616a49e5db57c1e8f3d960f062697b4d0fc24
Hamza Rabie <hamza.rabie1997@gmail.com>
2023-04-11 16:44:26 +0300

    modified portfolio

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -12,6 +12,15 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 
+from environ import Env
+env = Env()
+# reading .env file
+env.read_env()
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'MY_SCRET_KEY'
+
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +29,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p(k2%jl2#(38#m&88x#5pzbqjctqqs-vb=72&cy+2eog9w%ko&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
